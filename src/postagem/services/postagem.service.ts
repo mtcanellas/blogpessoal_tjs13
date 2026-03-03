081eb22f00bb82aa1a883ede5c521d99d0344ecd
Matheus Canellas <mtcanellas@gmail.com>
2026-03-03 13:07:16 -0300

    ✨ New Feature:  Construção do  Recurso Postagem

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { ILike, Repository } from "typeorm";
 import { Postagem } from "../entities/postagem.entity";
+import { DeleteResult } from "typeorm/browser";
 
 
 @Injectable()
@@ -15,4 +16,48 @@ export class PostagemService{
         //SELECT * FROM  tb_postagens
         return this.postagemRepository.find();
     }
+
+    async findById(id: number) : Promise<Postagem>{
+        // SELECT * FROM tb_postagens WHERE id = ?;
+        const postagem = await this.postagemRepository.findOne({
+            where:{
+                    id}
+    })
+
+    if(!postagem) 
+        throw new HttpException('Postagem não encontrada!' , HttpStatus.NOT_FOUND)
+
+    return postagem;
+    }
+
+    async findAlllByTitulo(titulo: string) : Promise <Postagem[]>{
+        // SELECT * FROM tb_postagens WHERE titulo LIKE '%?%';
+        return this.postagemRepository.find({
+            where:{
+                titulo: ILike(`%${titulo}%`) //Iike (ignora maiusculo e minusculo )
+            }
+    })
+    }
+
+    async create(postagem: Postagem) : Promise<Postagem>{
+        return await this.postagemRepository.save(postagem);
+    }
+
+     
+    async update(postagem: Postagem) : Promise<Postagem>{
+        if(!postagem.id || postagem.id <=0 )
+            throw new HttpException("O ID da postagem é inválido!" , HttpStatus.BAD_REQUEST);
+        await this.findById(postagem.id);
+
+        return this.postagemRepository.save(postagem);
+    }
+
+    async delete(id: number) : Promise<DeleteResult>{
+        await this.findById(id);
+        
+        //Delete tb_postagens FROM id = ?
+        return this.postagemRepository.delete(id);
+    }
+
+
 }
